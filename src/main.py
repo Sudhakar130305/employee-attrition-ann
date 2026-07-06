f01bb38bb8732dbf3529e1ba44cec5709d90d17c
sudhakar <sudhakar130305@gmail.com>
2026-07-06 14:26:29 +0530

    Project setup and dataset loading

---
+++ src/main.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+df = pd.read_csv("data/WA_Fn-UseC_-HR-Employee-Attrition.csv")
+print(df.head())
+print("\nShape of Dataset:")
+print(df.shape)
+
+print("\nColumn Names:")
+print(df.columns)
+
+print("\nDataset Information:")
+df.info()
+
+print("\nMissing Values:")
+print(df.isnull().sum())
+
+print("\nTarget Distribution:")
+print(df["Attrition"].value_counts())
